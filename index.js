5e5720ffbe075096d1fb70ab621458a802b47f8c
Richard Harrah <topplethenunnery@gmail.com>
2018-09-28 22:52:14 -0400

    adding a log for equipping a tool

--- index.js
+++ index.js
@@ -382,6 +382,7 @@ function inject(bot) {
       return toolMultipliers[b.type] - toolMultipliers[a.type];
     });
     var tool = tools[0];
+    console.log('Equipping tool: ' + tool);
     if (!tool) return true;
     if (bot.heldItem && bot.heldItem.type === tool.type) return true;
     var done = false;
