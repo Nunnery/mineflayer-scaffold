1c650f89008fc6ea23ddf90659a1916b2f28f76f
Richard Harrah <topplethenunnery@gmail.com>
2018-09-28 22:45:50 -0400

    updating to newest mineflayer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var EventEmitter = require('events').EventEmitter
-  , assert = require('assert')
+var EventEmitter = require('events').EventEmitter,
+  assert = require('assert'),
+  vec3 = require('vec3');
 
 module.exports = init;
 
@@ -10,14 +11,14 @@ var materials;
 
 // block types allowed to be used as scaffolding
 var scaffoldBlockTypes = {
-  3:  true, // dirt
-  4:  true, // cobblestone
+  3: true, // dirt
+  4: true, // cobblestone
   87: true, // netherrack
 };
 
 var liquidBlockTypes = {
-  8: true,  // water
-  9: true,  // water
+  8: true, // water
+  9: true, // water
   10: true, // lava
   11: true, // lava
 };
@@ -28,14 +29,13 @@ var fallingBlockTypes = {
 };
 
 function init(mineflayer) {
-  vec3 = mineflayer.vec3;
   sideVecs = [
-    vec3(-1,  0,  0),
-    vec3( 1,  0,  0),
-    vec3( 0, -1,  0),
-    vec3( 0,  1,  0),
-    vec3( 0,  0, -1),
-    vec3( 0,  0,  1),
+    vec3(-1, 0, 0),
+    vec3(1, 0, 0),
+    vec3(0, -1, 0),
+    vec3(0, 1, 0),
+    vec3(0, 0, -1),
+    vec3(0, 0, 1),
   ];
   materials = mineflayer.materials;
   return inject;
@@ -58,25 +58,27 @@ function inject(bot) {
   // is cumbersome to type
   var targetPoint;
 
-  bot.on('death', function() {
+  bot.on('death', function () {
     changeState('off', 'death');
   });
 
   var transition = {
     walk: function () {
       // try to get to destination block
-      var results = bot.navigate.findPathSync(targetPoint, { timeout: navigateTimeout, });
+      var results = bot.navigate.findPathSync(targetPoint, {
+        timeout: navigateTimeout,
+      });
 
       // we don't even care if it worked. just get close and then re-evaluate.
       var done = false;
-      bot.navigate.walk(results.path, function() {
+      bot.navigate.walk(results.path, function () {
         if (done) return;
         done = true;
         moveToBlockCenter();
         changeState('improvePosition');
       });
       changeState('walking');
-      cleanups.push(function() {
+      cleanups.push(function () {
         if (done) return;
         done = true;
         bot.navigate.stop();
@@ -118,13 +120,13 @@ function inject(bot) {
       if (!bot.entity.onGround) {
         // we're falling. nothing to do except wait.
         var done = false;
-        setTimeout(function() {
+        setTimeout(function () {
           if (done) return;
           done = true;
           changeState('improvePosition');
         }, 500);
         changeState('falling');
-        cleanups.push(function() {
+        cleanups.push(function () {
           if (done) return;
           done = true;
         });
@@ -139,17 +141,18 @@ function inject(bot) {
           return;
         }
       }
-      if (! equipBuildingBlock()) return;
+      if (!equipBuildingBlock()) return;
       // jump and build a block down below
       bot.setControlState('jump', true);
       var targetBlock = bot.blockAt(bot.entity.position.offset(0, -1, 0));
       var jumpY = bot.entity.position.y + 1;
       bot.on('move', placeIfHighEnough);
       changeState('jumping');
-      cleanups.push(function() {
+      cleanups.push(function () {
         bot.removeListener('move', placeIfHighEnough);
         bot.setControlState('jump', false);
       });
+
       function placeIfHighEnough() {
         if (bot.entity.position.y > jumpY) {
           bot.placeBlock(targetBlock, vec3(0, 1, 0));
@@ -163,19 +166,19 @@ function inject(bot) {
       if (!bot.entity.onGround || groundBlock.boundingBox === 'empty') {
         // we're falling. nothing to do except wait.
         var done = false;
-        setTimeout(function() {
+        setTimeout(function () {
           if (done) return;
           done = true;
           changeState('improvePosition');
         }, 500);
         changeState('falling');
-        cleanups.push(function() {
+        cleanups.push(function () {
           if (done) return;
           done = true;
         });
         return;
       }
-      if (! groundBlock.diggable) {
+      if (!groundBlock.diggable) {
         // let's try improving Z or X first.
         improveX();
         return;
@@ -184,7 +187,7 @@ function inject(bot) {
       var nextGroundBlock = bot.blockAt(bot.entity.position.offset(0, -2, 0));
       if (nextGroundBlock.boundingBox !== 'block') {
         if (nextGroundBlock.type === 0) {
-          if (! placeBlock(groundBlock, vec3(0, -1, 0))) return;
+          if (!placeBlock(groundBlock, vec3(0, -1, 0))) return;
         } else {
           breakBlock(nextGroundBlock, "decreaseY");
           return;
@@ -208,7 +211,7 @@ function inject(bot) {
   }
 
   function to(point, options, cb) {
-    if (! cb) {
+    if (!cb) {
       cb = options;
       options = {};
     }
@@ -232,7 +235,9 @@ function inject(bot) {
   function changeState(newState, reason, data) {
     var oldState = bot.scaffold.state;
     bot.scaffold.state = newState;
-    cleanups.forEach(function(fn) { fn(); });
+    cleanups.forEach(function (fn) {
+      fn();
+    });
     cleanups = [];
     bot.scaffold.emit('changeState', oldState, newState, reason, data);
     if (newState === 'off') {
@@ -248,11 +253,13 @@ function inject(bot) {
     }
     transition[newState]();
   }
+
   function moveToBlockCenter() {
     bot.entity.velocity.set(0, bot.entity.velocity.y, 0);
     var centerPos = bot.entity.position.floored().offset(0.5, 0.5, 0.5);
     bot.entity.position.set(centerPos.x, bot.entity.position.y, centerPos.z);
   }
+
   function improveX() {
     moveToBlockCenter();
     var flooredX = Math.floor(bot.entity.position.x);
@@ -265,6 +272,7 @@ function inject(bot) {
     // we are at the target point. mission accomplished.
     changeState('off', 'success');
   }
+
   function moveInDirection(dir) {
     // if the 3 blocks are in place such that we can move into the new
     // location, do it.
@@ -277,20 +285,20 @@ function inject(bot) {
     } else if (upper.boundingBox !== 'empty') {
       breakBlock(upper, bot.scaffold.state);
     } else if (floor.boundingBox === 'empty') {
-      if (! equipBuildingBlock()) return;
+      if (!equipBuildingBlock()) return;
       var myFloor = bot.blockAt(bot.entity.position.offset(0, -1, 0));
-      if (! placeBlock(myFloor, dir)) return;
+      if (!placeBlock(myFloor, dir)) return;
       changeState('improvePosition');
     } else {
       var done = false;
-      bot.navigate.walk([newPos], function(stopReason) {
+      bot.navigate.walk([newPos], function (stopReason) {
         if (done) return;
         done = true;
         moveToBlockCenter();
         changeState('improvePosition');
       });
       changeState('walking');
-      cleanups.push(function() {
+      cleanups.push(function () {
         if (done) return;
         done = true;
         bot.navigate.stop();
@@ -298,29 +306,30 @@ function inject(bot) {
       });
     }
   }
+
   function breakBlock(targetBlock, newState) {
     // before breaking, plug up any lava or water
-    var dangerBlockAndVecs = sideVecs.map(function(sideVec) {
+    var dangerBlockAndVecs = sideVecs.map(function (sideVec) {
       return {
         block: bot.blockAt(targetBlock.position.plus(sideVec)),
         sideVec: sideVec,
       };
-    }).filter(function(sideBlockAndVec) {
+    }).filter(function (sideBlockAndVec) {
       return liquidBlockTypes[sideBlockAndVec.block.type];
     });
     for (var i = 0; i < dangerBlockAndVecs.length; ++i) {
-      if (! placeBlock(targetBlock, dangerBlockAndVecs[i].sideVec)) return;
+      if (!placeBlock(targetBlock, dangerBlockAndVecs[i].sideVec)) return;
     }
     var aboveBlock = bot.blockAt(targetBlock.position.offset(0, 1, 0));
     var fallDanger = !!fallingBlockTypes[aboveBlock.type];
 
-    if (! bot.canDigBlock(targetBlock) || fallDanger) {
+    if (!bot.canDigBlock(targetBlock) || fallDanger) {
       changeState('off', 'danger', targetBlock);
       return;
     }
-    if (! equipToolToBreak(targetBlock)) return;
+    if (!equipToolToBreak(targetBlock)) return;
     var done = false;
-    bot.dig(targetBlock, function(err) {
+    bot.dig(targetBlock, function (err) {
       if (done) return;
       done = true;
       if (err) {
@@ -334,24 +343,26 @@ function inject(bot) {
       }
     });
     changeState('digging');
-    cleanups.push(function() {
+    cleanups.push(function () {
       if (done) return;
       done = true;
     });
   }
+
   function canHarvest(block) {
     var okTools = block.harvestTools;
     if (!okTools) return true;
     if (bot.heldItem && okTools[bot.heldItem.type]) return true;
     // see if we have the tool necessary in inventory
-    var tools = bot.inventory.items().filter(function(item) {
+    var tools = bot.inventory.items().filter(function (item) {
       return okTools[item.type];
     });
     var tool = tools[0];
     return !!tool;
   }
+
   function equipToolToBreak(blockToBreak) {
-    if (! canHarvest(blockToBreak)) {
+    if (!canHarvest(blockToBreak)) {
       changeState('off', 'itemRequired', {
         types: blockToBreak.harvestTools,
         type: 'tool',
@@ -361,20 +372,20 @@ function inject(bot) {
     }
     // equip the most efficient tool that we have
     var material = blockToBreak.material;
-    if (! material) return true;
+    if (!material) return true;
     var toolMultipliers = materials[material];
     assert.ok(toolMultipliers);
-    var tools = bot.inventory.items().filter(function(item) {
+    var tools = bot.inventory.items().filter(function (item) {
       return toolMultipliers[item.type] != null;
     });
-    tools.sort(function(a, b) {
+    tools.sort(function (a, b) {
       return toolMultipliers[b.type] - toolMultipliers[a.type];
     });
     var tool = tools[0];
     if (!tool) return true;
     if (bot.heldItem && bot.heldItem.type === tool.type) return true;
     var done = false;
-    bot.equip(tool, 'hand', function(err) {
+    bot.equip(tool, 'hand', function (err) {
       if (done) return;
       done = true;
       if (err) {
@@ -384,20 +395,22 @@ function inject(bot) {
       }
     });
     changeState('equipTool');
-    cleanups.push(function() {
+    cleanups.push(function () {
       if (done) return;
       done = true;
     });
   }
+
   function placeBlock(referenceBlock, dir) {
-    if (! equipBuildingBlock()) return false;
+    if (!equipBuildingBlock()) return false;
     bot.placeBlock(referenceBlock, dir);
     return true;
   }
+
   function equipBuildingBlock() {
     // return true if we're already good to go
     if (bot.heldItem && scaffoldBlockTypes[bot.heldItem.type]) return true;
-    var scaffoldingItems = bot.inventory.items().filter(function(item) {
+    var scaffoldingItems = bot.inventory.items().filter(function (item) {
       return scaffoldBlockTypes[item.type];
     });
     var item = scaffoldingItems[0];
@@ -409,7 +422,7 @@ function inject(bot) {
       return false;
     }
     var done = false;
-    bot.equip(scaffoldingItems[0], 'hand', function(err) {
+    bot.equip(scaffoldingItems[0], 'hand', function (err) {
       if (done) return;
       done = true;
       if (err) {
@@ -419,7 +432,7 @@ function inject(bot) {
       }
     });
     changeState('equipBuildingBlock');
-    cleanups.push(function() {
+    cleanups.push(function () {
       if (done) return;
       done = true;
     });
